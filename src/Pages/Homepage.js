cdce77773a430813b57a8fda10e7ab249a681765
nicholassitoRMIT <s3951974@student.rmit.edu.au>
2023-08-30 14:27:38 +1000

    changed image name

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,12 +1,16 @@
 import React from "react"
+import imgBike from './images/imgBike.jpg'
 
 //TODO: UNFINISHED
 
 function Homepage(){
     return(
-        <h1>
-            Currently Showing Movies
-        </h1>
+        <div>
+            <h1>
+                Currently Showing Movies
+            </h1>
+
+        </div>
     )
 }
 
