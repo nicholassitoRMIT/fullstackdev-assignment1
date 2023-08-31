e39fea2c50bd37d5e2ae6a4952c4bcb83bbaa5b2
nicholassitoRMIT <s3951974@student.rmit.edu.au>
2023-08-31 14:09:04 +1000

    Added images

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -9,8 +9,22 @@ function Homepage(){
                 Currently Showing Movies
             </h1>
 
-            <img src={'./images/imgBike.jpg'} className="imgBike" alt=""/>
+            <img src={'./images/imgBike.jpg'} className="imgBike" width="800" height="350" alt=""/>
 
+            <div className="row1">
+                <div className="imgRow1">
+                    <img src={'./images/imgClimber.jpg'} alt="Climber"></img>
+                </div>
+
+                <div className="imgRow1">
+                    <img src={'./images/imgCoolWalk.jpg'} alt="BadassWalk"></img>
+                </div>
+
+                <div className="imgRow1">
+                    <img src={'./images/imgDancer.jpg'} alt="Dancer"></img>
+                </div>
+
+            </div>
         </div>
     )
 }
