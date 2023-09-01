477d975f4a17a9ae79cc43bf2bcd769a3ef291b4
nicholassitoRMIT <s3951974@student.rmit.edu.au>
2023-09-01 15:40:44 +1000

    Completed footer and styled links

--- src/Pages/Components/Footer.js
+++ src/Pages/Components/Footer.js
@@ -5,7 +5,16 @@ import React from "react";
 function Footer(){
     return (
         <div className="footer">
-            <p className="footer-text">Loop cinemas footer thing</p>
+            <h1 className="footer-header">Contact Us</h1>
+
+            <div className="footer-text">
+                <p><a href="https://www.youtube.com/" target="_blank">YouTube</a></p>
+                <p><a href="https://www.facebook.com/" target="_blank">Facebook</a></p>
+                <p><a href="https://www.instagram.com/" target="_blank">Instagram</a></p>
+                <p><a href="https://www.twitter.com/" target="_blank">Twitter</a></p>
+                <p><a href="https://outlook.live.com/" target="_blank">Email</a></p>
+            </div>
+
         </div>
     )
 }
