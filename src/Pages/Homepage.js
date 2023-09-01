9769fa0e9080edd5e54d3b76d0c8b5364abd9bb4
nicholassitoRMIT <s3951974@student.rmit.edu.au>
2023-09-01 17:35:59 +1000

    Content added

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -5,28 +5,57 @@ import React from "react"
 function Homepage(){
     return(
         <div>
-            <h1>
-                Currently Showing Movies
-            </h1>
+            <div className="introduction">
+                <h1><b>Why Loop Cinemas?</b></h1>
+                <p>Loop Cinemas is a chain of cinemas showing the latest movies in high quality at locations all across Australia. Here at Loop Cinemas, we value community interaction, therefore we offer a unique service that allows you to interact with your fellow watchers by leaving reviews. Your feedback and support is valuable to us.</p>
+            </div>
 
-            <img src={'./images/imgBike.jpg'} className="imgBike" width="800" height="350" alt=""/>
-
+            <h1>Currently Showing</h1>
+            
             <div className="row1">
                 <div className="imgRow1">
                     <img src={'./images/imgClimber.jpg'} className="Climber" alt="ClimberImage"></img>
                     <h2>The Climber</h2>
+                    <p>12:00 - 13:30</p>
+                    <p>14:00 - 15:30</p>
                 </div>
 
                 <div className="imgRow1">
                     <img src={'./images/imgCoolWalk.jpg'} className="BadassWalk" alt="WalkerImage"></img>
                     <h2>Crossroads</h2>
+                    <p>12:30 - 14:00</p>
+                    <p>17:00 = 18:30</p>
                 </div>
 
                 <div className="imgRow1">
                     <img src={'./images/imgDancer.jpg'} className="Dancer" alt="DancerImage"></img>
                     <h2>Dance Dance Revolution</h2>
+                    <p>11:00 - 13:00</p>
+                    <p>16:00 - 18:00</p>
                 </div>
-                
+            </div>
+
+            <div className="row2">
+                <div className="imgRow2">
+                    <img src={"./images/imgSkiier.jpg"} className="Skiier" alt="SkiierImage"></img>
+                    <h2>The Skiier</h2>
+                    <p>10:30 - 12:00</p>
+                    <p>12:30 - 14:00</p>
+                </div>
+
+                <div className="imgRow2">
+                    <img src={"./images/imgBaseball.jpg"} className="Baseball" alt="BaseballImage"></img>
+                    <h2>Champion's Run</h2>
+                    <p>11:00 - 13:00</p>
+                    <p>15:00 - 17:00</p>
+                </div>
+
+                <div className="imgRow2">
+                    <img src={"./images/imgFootball.jpg"} className="Football" alt="FootballImage"></img>
+                    <h2>Kickout</h2>
+                    <p>11:00 - 12:30</p>
+                    <p>13:30 - 15:00</p>
+                </div>
             </div>
         </div>
     )
